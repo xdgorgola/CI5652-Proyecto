e67be9aac9122209d281dfb4f685e94e3c0c5fde
PanquecaFuriosa <18-10761@usb.ve>
2024-06-25 23:05:43 -0400

    fixing ant colony optimization

--- MVC_functions/ACOMVC.py
+++ MVC_functions/ACOMVC.py
@@ -17,12 +17,10 @@ def travel_graph(g: Graph, source: int, pheromones: list[int]) -> set[int]:
         if edge[0] != source and edge[1] != source:
             uncovered_edges.append(edge)
 
-    current: int = source
     while len(uncovered_edges) != 0:
         jumps_neighbors: list[int] = []
         jumps_values: list[float] = []
-
-        for v in g.vertex_count:
+        for v in range(g.vertex_count):
             if not visited[v]:
                 sediment = max(pheromones[v], 1e-5)
                 value = (sediment**0.9 ) * (g.get_degree(v) **1.5) 
@@ -31,11 +29,22 @@ def travel_graph(g: Graph, source: int, pheromones: list[int]) -> set[int]:
 
         next_node = random.choices(jumps_neighbors, weights = jumps_values)[0]
         visited[next_node] = 1
-        current = next_node
-        cover.add(current)
-        for uncovered_edge in uncovered_edges:
-                if uncovered_edge[0] == next_node or uncovered_edge[1] == next_node:
-                    uncovered_edges.remove(uncovered_edge)
+        
+        any_cover: bool = False
+        indices_lados_eliminar = set()
+        
+        for i in range(len(uncovered_edges)):
+            if uncovered_edges[i][0] == next_node or uncovered_edges[i][1] == next_node:
+                if not any_cover:
+                    any_cover = True
+            
+                indices_lados_eliminar.add(i)
+
+        uncovered_edges = [uncovered_edges[i] for i in range(len(uncovered_edges)) if i not in indices_lados_eliminar]
+
+        if any_cover:
+            cover.add(next_node)
+
     return cover
 
 def ACO(g: Graph, 
@@ -46,14 +55,16 @@ def ACO(g: Graph,
     C: set[int] = set(range(g.vertex_count))
     C_star: set[int] = type(C)(C)
     pheromones: list[float] = [1e-2]*g.vertex_count
+    found_time: float | None = None
+    found_time_aux: float | None = None
 
     start_time: float = time.time()
     if q is None:
         q = (3/4) * g.vertex_count
 
     while time.time() - start_time < cutoff:
-
         covers: list[set[int]] = [travel_graph(g, random.randint(0, g.vertex_count - 1), pheromones) for _ in range(ant_count)]
+        found_time_aux = time.time()
         covers.sort(key=lambda x: len(x))
         len_best_cover = len(covers[0])
 
@@ -62,9 +73,10 @@ def ACO(g: Graph,
             for v in cover:
                 pheromones[v] += delta
 
-        pheromones *= degradation_factor
+        pheromones = [i * degradation_factor for i in pheromones]
 
         if len_best_cover < len(C_star):
+            found_time = found_time_aux - start_time
             C_star = type(covers[0])(covers[0])
 
-    return C_star
+    return (C_star, found_time)
--- MVC_functions/__init__.py
+++ MVC_functions/__init__.py
@@ -2,3 +2,4 @@ from .brute_forceMVC import *
 from .fastVC import *
 from .local_searchMVC import *
 from .geneticVC import *
+from .ACOMVC import ACO
